3bb7def1abfe14d01a77b01274e344af9031d91b
PENG, LIHENG <liheng@6estates.com>
2019-10-10 17:40:45 +0800

    feature: finish add frontend hmr

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,13 @@
-"use strict";
-
+import 'babel-polyfill'
 
 (async() => {
-  const res = new Promise((resolve, reject) => {
+  const res = await new Promise((resolve, reject) => {
     fetch('http://localhost:5000')
       .then(res => res.json())
       .then(res => resolve(res))
       .catch(err => reject(err))
   })
-  document.body.innerHTML = await res
+  document.body.innerHTML = JSON.stringify(res)
 })();
+
+// WebSocket connection to 'ws://localhost:46263/' failed: Error in connection establishment: net::ERR_CONNECTION_REFUSED
