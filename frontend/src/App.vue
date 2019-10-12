46d2d81306842012c798cc155fce58da9b40ab7e
pengliheng <pengliheng111@gmail.com>
2019-10-12 15:21:18 +0000

    feature: support to use parcel to relize hot reload and support ts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,29 @@
-<template>
-  <div id="app">
-    <router-view />
-  </div>
+<template lang="pug">
+  .container 
+    .left left {{message}}
+      button(@click="greet") {click me}
+    .middle middle
+    .right right
+
 </template>
 
-<script>
-export default {
-  name: 'App'
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({})
+export default class MyComponent extends Vue {
+  message: string = "Hello!";
+
+  // method
+  greet() {
+    alert("greeting: " + this.message);
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.container {
+  color: red;
+}
+</style>
